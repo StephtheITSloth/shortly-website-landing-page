68fb089c369247c2d274be7429a044f5da5a42e4
stephtheitsloth <stephan.karim.sk@gmail.com>
2023-12-28 18:18:04 -0500

    navbar and toggle menu

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
-const btn = document.getElementById('menu-btn')
+const hamburgerBtn = document.getElementById('hamburger-btn')
 const menu = document.getElementById('menu')
 
-const input = document.getElementById('link-input')
-const linkForm = document.getElementById('link-form')
-const errMsg = document.getElementById('err-msg')
+hamburgerBtn.addEventListener('click', toggleMenu)
 
-btn.addEventListener('click', navToggle)
-linkForm.addEventListener('submit', formSubmit)
-
-// Toggle Mobile Menu
-function navToggle() {
-  btn.classList.toggle('open')
-  menu.classList.toggle('flex')
+function toggleMenu(){
+  hamburgerBtn.classList.toggle('open')
   menu.classList.toggle('hidden')
+  menu.classList.toggle('flex')
 }
+
+
 
 // Validate a URL
 function validURL(str) {
@@ -27,20 +23,4 @@ function validURL(str) {
     'i'
   )
   return !!pattern.test(str)
-}
-
-function formSubmit(e) {
-  e.preventDefault()
-
-  if (input.value === '') {
-    errMsg.innerHTML = 'Please enter something'
-    input.classList.add('border-red')
-  } else if (!validURL(input.value)) {
-    errMsg.innerHTML = 'Please enter a valid URL'
-    input.classList.add('border-red')
-  } else {
-    errMsg.innerHTML = ''
-    input.classList.remove('border-red')
-    alert('Success')
-  }
 }
